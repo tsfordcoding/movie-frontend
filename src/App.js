5b005055af8354f634e1acb0fb32763bb814fd26
Timothy Ford <71365012+tsfordcoding@users.noreply.github.com>
2023-08-05 17:47:16 -0700

    Finished movies project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./shared/Header";
+import MoviesList from "./home/MoviesList";
+import DetailedMoviesList from "./movies/DetailedMoviesList";
+import FullMovie from "./movie/FullMovie";
+import TheaterList from "./theaters/TheaterList";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <MoviesList />
+        </Route>
+        <Route exact path="/movies">
+          <DetailedMoviesList />
+        </Route>
+        <Route exact path="/movies/:movieId">
+          <FullMovie />
+        </Route>
+        <Route exact path="/theaters">
+          <TheaterList />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
